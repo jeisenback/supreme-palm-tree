bf2a88af25462e9788ab64372c60342eb6351d62
jeisenback <jeisenback@gmail.com>
2026-03-15 14:16:03 -0400

    feat(scraper): add PartnerScraper and tests

--- ingest/scrapers/__init__.py
+++ ingest/scrapers/__init__.py
@@ -2,5 +2,6 @@
 
 from .event_scraper import EventScraper
 from .job_scraper import JobScraper
+from .partner_scraper import PartnerScraper
 
-__all__ = ["EventScraper", "JobScraper"]
+__all__ = ["EventScraper", "JobScraper", "PartnerScraper"]
